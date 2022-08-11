083e9a1b3f723bc99a6d0bc07496551b5816f871
Emre Can Berktaş <emrecanberktas@gmail.com>
2022-08-11 23:15:36 +0300

    minor updates

--- src/pages/Films.js
+++ src/pages/Films.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Films() {
-  return <div>Films</div>;
+  const [page, setPage] = React.useState(1);
+  const [films, setFilms] = React.useState([]);
+  const getPopularFilms = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=2b4d9cd70e38584279f12e85a03dc37a&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        console.log(res);
+        setFilms(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getPopularFilms();
+  }, [page]);
+  return <div>{}Films</div>;
 }
 
 export default Films;
