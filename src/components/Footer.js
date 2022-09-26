f4487fa4a71d12ca00e9f090c28543e22de21b2f
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-09-26 15:53:05 +0300

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <br />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Made By Emre Can Berktas
+      </div>
+      <br />
+    </div>
+  );
+}
+
+export default Footer;
